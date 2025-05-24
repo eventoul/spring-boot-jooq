11db22944f0502cef48749b645b44039adacd9e9
eventoul <bagb88@gmail.com>
2025-05-25 00:33:07 +0300

    feat: delete student by id.

--- src/main/java/com/jooq/springbootjooq/service/StudentServiceImpl.java
+++ src/main/java/com/jooq/springbootjooq/service/StudentServiceImpl.java
@@ -19,21 +19,23 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public List<Student> selectAll() {
-
         return dsl.selectFrom(STUDENT).fetchInto(Student.class);
     }
 
     @Override
     public List<Student> selectById(Integer id) {
-
         return dsl.selectFrom(STUDENT).where(STUDENT.ID.eq(id)).fetchInto(Student.class);
     }
 
     @Override
     public void saveStudent(Student student) {
-
         dsl.insertInto(STUDENT, STUDENT.ID, STUDENT.EMAIL, STUDENT.FIRST_NAME, STUDENT.LAST_NAME)
                 .values(student.getId(), student.getEmail(), student.getFirstName(), student.getLastName())
                 .execute();
+    }
+
+    @Override
+    public void deleteStudent(Integer id) {
+        dsl.deleteFrom(STUDENT).where(STUDENT.ID.eq(id)).execute();
     }
 }
