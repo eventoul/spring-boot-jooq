28254e176b92a630c79eb9bc684fc31463e78878
eventoul <bagb88@gmail.com>
2025-05-26 23:36:02 +0300

    feat: update a course.

--- src/main/java/com/jooq/springbootjooq/controller/CourseController.java
+++ src/main/java/com/jooq/springbootjooq/controller/CourseController.java
@@ -3,6 +3,7 @@ package com.jooq.springbootjooq.controller;
 import com.jooq.springbootjooq.generated.tables.pojos.Course;
 import com.jooq.springbootjooq.service.CourseService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -49,5 +50,15 @@ public class CourseController {
         courseService.deleteCourse(courseId);
 
         return "Deleted course id: " + courseId;
+    }
+
+    @PutMapping("/update/{courseId}")
+    public ResponseEntity<?> updateCourse(@PathVariable Integer courseId, @RequestBody Course course) {
+
+        log.warn("course: {}", course);
+
+        courseService.updateCourse(courseId, course);
+
+        return ResponseEntity.ok(course);
     }
 }
